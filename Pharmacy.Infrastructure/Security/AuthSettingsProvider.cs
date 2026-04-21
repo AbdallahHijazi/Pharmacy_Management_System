5388784c92bf18c08e5c8ec79c73232aab8500fe
Abdallah_Hijazi <AbdallahHijazi666@gmail.com>
2026-04-21 17:57:24 +0300

    feat(auth): implement login flow with JWT authentication

---
+++ Pharmacy.Infrastructure/Security/AuthSettingsProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Pharmacy.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Infrastructure.Security
+{
+    public class AuthSettingsProvider : IAuthSettingsProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthSettingsProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Secret => _configuration["Jwt:Secret"]!;
+        public string Issuer => _configuration["Jwt:Issuer"]!;
+        public string Audience => _configuration["Jwt:Audience"]!;
+        public int ExpiryMinutes => int.Parse(_configuration["Jwt:ExpiryMinutes"]!);
+    }
+}
