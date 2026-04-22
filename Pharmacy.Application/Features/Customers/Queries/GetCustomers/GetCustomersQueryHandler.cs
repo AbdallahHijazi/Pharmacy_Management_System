0caf5fe8650ff8a41e4b814a10bae9ab0e7a57f3
Abdallah_Hijazi <AbdallahHijazi666@gmail.com>
2026-04-22 04:49:37 +0300

    feat(customers): add get customers endpoint

---
+++ Pharmacy.Application/Features/Customers/Queries/GetCustomers/GetCustomersQueryHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Application.Common.Interfaces;
+using Pharmacy.Application.DTOs.Customers;
+using Pharmacy.Domain.Entities.Partners;
+using Pharmacy.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Application.Features.Customers.Queries.GetCustomers
+{
+    public class GetCustomersQueryHandler : IRequestHandler<GetCustomersQuery, List<CustomerListItemDto>>
+    {
+        private readonly IRepository<Customer> _customerRepository;
+        private readonly ICurrentUserService _currentUserService;
+
+        public GetCustomersQueryHandler(
+            IRepository<Customer> customerRepository,
+            ICurrentUserService currentUserService)
+        {
+            _customerRepository = customerRepository;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<List<CustomerListItemDto>> Handle(GetCustomersQuery request, CancellationToken cancellationToken)
+        {
+            if (_currentUserService.UserId is null)
+                throw new UnauthorizedException("المستخدم غير مسجل الدخول");
+
+            if (_currentUserService.BranchId is null)
+                throw new UnauthorizedException("لا يوجد فرع مرتبط بالمستخدم الحالي");
+
+            var customers = await _customerRepository
+                .GetAll()
+                .Where(c => !c.IsDeleted && c.BranchId == _currentUserService.BranchId.Value)
+                .OrderBy(c => c.FullName)
+                .Select(c => new CustomerListItemDto
+                {
+                    CustomerId = c.Id,
+                    FullName = c.FullName,
+                    Phone = c.Phone,
+                    Address = c.Address,
+                    TotalPurchases = c.TotalPurchases,
+                    DebtAmount = c.DebtAmount,
+                    BranchId = c.BranchId
+                })
+                .ToListAsync(cancellationToken);
+
+            return customers;
+        }
+    }
+}
---
+++ PharmacyProjectApi/Controllers/Customers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy.API.Infrastructure;
+using Pharmacy.Application.DTOs.Customers;
+using Pharmacy.Application.Features.Customers.Queries.GetCustomers;
+
+namespace PharmacyProjectApi.Controllers.Customers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/v1/customers")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CustomersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<CustomerListItemDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var result = await _mediator.Send(new GetCustomersQuery());
+            return Ok(result);
+        }
+    }
+}
